7cc30f235d7ee0dbab4a8f108256081cd2f411b4
Voronov Dmitrii <VoronovDmitrii98@yandex.ru>
2024-03-21 14:46:27 +0300

    add insert count item

--- widget.cpp
+++ widget.cpp
@@ -11,24 +11,24 @@ Widget::Widget(QWidget *parent)
     QLabel *lab1=new QLabel("Введите размер листа");
     linef=new QLineEdit;
     lines=new QLineEdit;
-    QLabel *lab7=new QLabel("Введите количество деталей");
+    QLabel *lab7=new QLabel("Введите количество строк ввода");
     linecou=new QLineEdit;
      QPushButton *button2=new QPushButton("ввод");
       QWidget::connect(button2,&QPushButton::clicked,this,&Widget::setcount);
-    QLabel *lab2=new QLabel("Введите размер деталей");
-    table=new QTableWidget(0,2);
+    QLabel *lab2=new QLabel("Введите размер деталей(1,2) и их количество(3)");
+    table=new QTableWidget(0,3);
     QLabel *lab3=new QLabel("Вывод вставленных деталей");
     text1=new QTextEdit;
     text1->setReadOnly(true);
     QLabel *lab4=new QLabel("Вывод не вставленных деталей");
      text2=new QTextEdit;
      text2->setReadOnly(true);
-    linef->setMaximumSize(230,20);
-    lines->setMaximumSize(230,20);
-    linecou->setMaximumSize(230,20);
-    text1->setMaximumSize(230,100);
-    text2->setMaximumSize(230,100);
-    table->setMaximumSize(230,100);
+    linef->setMaximumSize(240,20);
+    lines->setMaximumSize(240,20);
+    linecou->setMaximumSize(240,20);
+    text1->setMaximumSize(240,100);
+    text2->setMaximumSize(240,100);
+    table->setMaximumSize(240,100);
     QVBoxLayout *box=new QVBoxLayout;
 
     QPushButton *button=new QPushButton("Старт");
@@ -70,15 +70,15 @@ void Widget::paintEvent(QPaintEvent *event)
         painter.setPen(QPen(Qt::red));
         for(int i=10;i<it->size.first;i+=10)
         {
-           painter.drawLine(250+i,20,250+i,20+it->size.second);
+           painter.drawLine(270+i,20,270+i,20+it->size.second);
         }
         for(int i=10;i<it->size.second;i+=10)
         {
-            painter.drawLine(250,20+i,250+it->size.first,20+i);
+            painter.drawLine(270,20+i,270+it->size.first,20+i);
         }
         painter.setPen(QPen(Qt::black,3));
         for(auto it:vec)
-            painter.drawRect(250+it.xy.first,20+it.xy.second,it.size.first,it.size.second);
+            painter.drawRect(270+it.xy.first,20+it.xy.second,it.size.first,it.size.second);
     }
 QWidget::paintEvent(event);
 
@@ -99,20 +99,30 @@ void Widget::slot()
 
     for(int i=0;i<table->rowCount();i++)
     {
-        if(table->item(i,0)==0 || table->item(i,1)==0)
+        if(table->item(i,0)==0 || table->item(i,1)==0 || table->item(i,2)==0)
             break;
         str1=table->item(i,0)->text();
         str2=table->item(i,1)->text();
 
          fir=str1.toInt(),sec=str2.toInt();
-         if (min(fir,sec)<=0 || min(fir,sec) > plate.second || max(fir,sec)>plate.first)
+         int col=table->item(i,2)->text().toUInt();
+
+
+             if (min(fir,sec)<=0 || min(fir,sec) > plate.second || max(fir,sec)>plate.first)
          {
-             out.push_back({fir,sec});
+                 for(int i=0; i<col;i++)
+                     {
+                     out.push_back({fir,sec});
+                      }
          }
          else
          {
-             in.push_back({max(fir,sec),min(fir,sec)});
+                 for(int i=0; i<col;i++)
+                                  {
+                                   in.push_back({max(fir,sec),min(fir,sec)});
+                                   }
          }
+
     }
         sort(in.begin(), in.end(), [](const pairii& r1, const pairii& r2)
             {
@@ -143,7 +153,7 @@ void Widget::slot()
         str1+=",";
         }
          text2->setText(str1);
-         this->resize(250+plate.first,20+ plate.second);
+         this->resize(270+plate.first,20+ plate.second);
     this->update();
 }
 
